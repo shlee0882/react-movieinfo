8b3f003592a4283afcceeef015969e576b5f28f2
shlee0882 <shlee0882@gmail.com>
2019-04-10 00:18:21 +0900

    커밋

--- src/App.js
+++ src/App.js
@@ -3,28 +3,34 @@ import logo from './logo.svg';
 import './App.css';
 import Movie from './Movie'
 
-const movieTitles = [
-  "The Load of the Rings",
-  "Harry Potter",
-  "Star Wars",
-  "I am Sam"
-]
 
-const movieImages = [
-  "https://images-na.ssl-images-amazon.com/images/I/51uGVkvLkCL.jpg",
-  "http://imgc.allpostersimages.com/img/posters/harry-potter-candles_u-L-F9DGY50.jpg",
-  "https://imgc.allpostersimages.com/img/print/u-g-F8G58H0.jpg?w=300&h=450",
-  "https://m.media-amazon.com/images/M/MV5BMjE2Mzk5MTAxMV5BMl5BanBnXkFtZTcwNDQyMjgyMQ@@._V1_.jpg"
+
+const movies = [
+  {
+    title: "The Load of the Rings",
+    poster: "https://images-na.ssl-images-amazon.com/images/I/51uGVkvLkCL.jpg"
+  },
+  {
+    title: "Harry Potter",
+    poster: "http://imgc.allpostersimages.com/img/posters/harry-potter-candles_u-L-F9DGY50.jpg"
+  },
+  {
+    title: "Star Wars",
+    poster: "https://imgc.allpostersimages.com/img/print/u-g-F8G58H0.jpg?w=300&h=450"
+  },
+  {
+    title: "I am Sam",
+    poster: "https://m.media-amazon.com/images/M/MV5BMjE2Mzk5MTAxMV5BMl5BanBnXkFtZTcwNDQyMjgyMQ@@._V1_.jpg"
+  },
 ]
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Movie title={movieTitles[0]} poster={movieImages[0]} />
-        <Movie title={movieTitles[1]} poster={movieImages[1]} />
-        <Movie title={movieTitles[2]} poster={movieImages[2]} />
-        <Movie title={movieTitles[3]} poster={movieImages[3]} />
+        {movies.map((movie, index) => {
+          return <Movie title={movie.title} poster={movie.poster} key={index} />
+        })}
       </div>
     );
   }
