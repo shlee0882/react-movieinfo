ca3250536b9fec137066fa84c2d9c7780259d421
shlee0882 <shlee0882@gmail.com>
2019-04-11 23:32:48 +0900

    Loading state

--- src/App.js
+++ src/App.js
@@ -23,11 +23,21 @@ class App extends Component {
     setTimeout(()=>{
       this.setState({
         movies: [
-          // 이전 영화리스트는 그대로 두고 한개의 영화를 추가하라는 뜻
-          ...this.state.movies,
           {
-            title: "love actually",
-            poster: "https://images-na.ssl-images-amazon.com/images/I/71PSelgZYvL._SY606_.jpg"
+            title: "The Load of the Rings",
+            poster: "https://images-na.ssl-images-amazon.com/images/I/51uGVkvLkCL.jpg"
+          },
+          {
+            title: "Harry Potter",
+            poster: "http://imgc.allpostersimages.com/img/posters/harry-potter-candles_u-L-F9DGY50.jpg"
+          },
+          {
+            title: "Star Wars",
+            poster: "https://imgc.allpostersimages.com/img/print/u-g-F8G58H0.jpg?w=300&h=450"
+          },
+          {
+            title: "I am Sam",
+            poster: "https://m.media-amazon.com/images/M/MV5BMjE2Mzk5MTAxMV5BMl5BanBnXkFtZTcwNDQyMjgyMQ@@._V1_.jpg"
           }
         ]
       })
@@ -36,34 +46,20 @@ class App extends Component {
   }
 
   state = {
-    greeting : 'Hello!',
-    movies : [
-      {
-        title: "The Load of the Rings",
-        poster: "https://images-na.ssl-images-amazon.com/images/I/51uGVkvLkCL.jpg"
-      },
-      {
-        title: "Harry Potter",
-        poster: "http://imgc.allpostersimages.com/img/posters/harry-potter-candles_u-L-F9DGY50.jpg"
-      },
-      {
-        title: "Star Wars",
-        poster: "https://imgc.allpostersimages.com/img/print/u-g-F8G58H0.jpg?w=300&h=450"
-      },
-      {
-        title: "I am Sam",
-        poster: "https://m.media-amazon.com/images/M/MV5BMjE2Mzk5MTAxMV5BMl5BanBnXkFtZTcwNDQyMjgyMQ@@._V1_.jpg"
-      }
-    ]
   }
+
+  _renderMovies = () => {
+    const movies = this.state.movies.map((movie, index) => {
+      return <Movie title={movie.title} poster={movie.poster} key={index} />
+    })
+    return movies
+  }
+
   render() {
     console.log('did render')
     return (
       <div className="App">
-        {this.state.greeting}
-        {this.state.movies.map((movie, index) => {
-          return <Movie title={movie.title} poster={movie.poster} key={index} />
-        })}
+        {this.state.movies ? this._renderMovies() : 'Loading'}
       </div>
     );
   }
