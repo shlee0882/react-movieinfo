0b1aefc09c0f52c02d68b5ca8e254058eb2460d4
shlee0882 <shlee0882@gmail.com>
2019-04-16 22:58:05 +0900

    Updates

---
+++ precache-manifest.97be3d61ac252702669d6743a8ff802c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "39f0687920de556be718",
+    "url": "/react-movieinfo/static/js/runtime~main.d161a6ff.js"
+  },
+  {
+    "revision": "e0449436d362a3213883",
+    "url": "/react-movieinfo/static/js/main.4a992d52.chunk.js"
+  },
+  {
+    "revision": "a6019cde03a925e9e4ae",
+    "url": "/react-movieinfo/static/js/2.27d4a8ee.chunk.js"
+  },
+  {
+    "revision": "e0449436d362a3213883",
+    "url": "/react-movieinfo/static/css/main.57293dc8.chunk.css"
+  },
+  {
+    "revision": "09703e63aacb81f1315757c43c059428",
+    "url": "/react-movieinfo/index.html"
+  }
+];
